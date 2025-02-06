fb08107f150a4361dc6b1d0cc7334f417609cec8
abroussard0711 <alexandria.broussard@g.austincc.edu>
2025-02-06 02:12:24 +0000

    hw1 multiply numbers

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,9 @@
-#
+#to execute the multiply_numbers function
+
+import output
+
+def main(): #defines the main function
+    result = output.multiply_numbers(6,6) #creates the result variable
+    print(result)   #prints the result
+
+main()  #runs the code
--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,7 @@
 def get_number(num):
     return num
+
+def multiply_numbers(num1, num2): #create a function named multiply_numbers that accepts two parameters with names num1 and num2
+    result = num1 * num2
+
+    return(result)  #returns the product of num1 and num2
