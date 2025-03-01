4617fefdef3031f0d6ad0f37aee41ccbef99d0cc
abroussard0711 <alexandria.broussard@g.austincc.edu>
2025-03-01 00:52:39 +0000

    more function examples

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -8,9 +8,16 @@ def main ():
     result = value_return_functions.echo_variable(num)
     print (result)
 
-    for i in range(0,20):
+    for i in range(0,2):
         results = value_return_functions.get_random_number(1,100)
         print (results)
+
+    lang = 'C++'
+    print (lang)
+    print (lang.isalpha())
+
+    lang = 'PYTHON'
+    print (lang.isupper())
 
 main ()
 
