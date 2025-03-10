3831fb33ea17ed59980695ae8d3f06d0789e86f1
abroussard0711 <alexandria.broussard@g.austincc.edu>
2025-03-10 12:56:36 +0000

    HW 5: Stings / Main.py Update - AB 03.08.25 GIT HUB Issue

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -3,16 +3,46 @@
 import strings
 
 def main():
-    
-    DNA1 = "GAGCCTACTAACGGGAT"
-    DNA2 = "CATCGTAATGACGGCCT"
+    result = run_menu()
+    print = result
 
-    print ("Hamming distance: ", strings.get_hamming_distance(DNA1,DNA2))
+# defines and displays the user input menu
+def display_menu():
+    print ('Homework 5 Menu')
+    print ('1-Hamming Distance')
+    print ('2-DNA Complement')
+    print ('3-Exit')
 
-    DNA = "AAAACCCGGT"
 
-    print ("Reverse compliment is: ", strings.get_dna_complement(DNA))
-    
+# defines and runs the user menu and prompt for inputs
+def run_menu():
+    user_option = '0'
+
+    while (user_option != '3'):
+        display_menu()
+
+        user_option = input ('Enter a menu option 1, 2, or 3: ')
+        handle_menu (user_option)
+
+
+# defines the input menu and its actionable items that reference the get_hamming_distance and get_dna_complement functions
+def handle_menu(user_option):
+
+    if (user_option == '1'):
+        s = input ("Enter a DNA sequence: ")
+        t = input ("Enter another DNA sequence: ")
+        result = strings.get_hamming_distance(s,t)
+        print ('The Hamming distance is = ', result)    
+      
+    elif (user_option == '2'):
+        seq = input ("Enter a DNA sequence: ")
+        result = strings.get_dna_complement(seq)
+        print ("The DNA Complement is: ", result)
+
+    elif (user_option == '3'):
+        print ('Exiting... ')
+
+    else: 
+        print ('Invalid menu option')
+
 main ()
-
-
