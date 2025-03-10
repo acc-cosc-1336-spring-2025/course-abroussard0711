3a4321a53e232c32cab5a87c7d8e107566da8a66
abroussard0711 <alexandria.broussard@g.austincc.edu>
2025-03-10 23:51:31 +0000

    Examples strings test code

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,9 +1,35 @@
 import unittest
 
-from src.examples.h_strings.strings import test_config
+from src.examples.h_strings.strings import string_params, string_return_value, test_config
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+
+    def test_string_params(self):
+        str1 = "Python"
+        
+        string_params(str1)
+        self.assertEqual(str1, "Python")
+        
+        str1 = "C++"
+        self.assertEqual(str1, "C++")
+
+    def test_string_return_value(self):
+        lang = "Python"
+        print(id(lang))
+
+        lang1 = string_return_value(lang)
+        print(id(lang1))
+
+        self.assertEqual(lang, "Python")
+        self.assertEqual(lang1, "C++")
+
+    def test_search_string_w_in(self):
+        text = "Four score and seven years ago"
+
+        is_in = 'Seven' in text
+
+        self.assertEqual (is_in, False)
 
