c7abf59a44eeff8ce05ac9d5af44c24d54b69afc
abroussard0711 <alexandria.broussard@g.austincc.edu>
2025-04-10 00:00:10 +0000

    Examples Ch 6 Working with Files

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,128 @@
 #
+
+import files
+
+def write_to_file (file_name):
+
+    file = open (file_name, 'w') # open a text file for writing
+
+    file.write ('John Locke\n')
+    file.write ('David Hume\n')
+    file.write ('Edmund Burke\n')
+
+    file.close ()
+
+
+def read_from_file (file_name):
+
+    file = open (file_name, 'r')
+
+    file_lines = file.read ()
+    file.close ()
+
+    print (file_lines)
+
+
+def read_from_file_one_line_at_a_time (file_name):
+    file = open (file_name, 'r')
+
+    line1 = file.readline ().rstrip ('\n')
+    line2 = file.readline ().rstrip ('\n')
+    line3 = file.readline ().rstrip ('\n')
+
+    file.close ()
+
+    print (line1)
+    print (line2)
+    print (line3)
+
+def read_from_file_data_w_while_loop (file_name):
+
+    file = open (file_name, 'r')
+
+    line = file.readline ().rstrip ('\n')
+    while (line != ''):
+        print (line)
+        line = file.readline ().rstrip ('\n')
+
+    file.close ()
+
+
+def read_from_file_data_w_for_loop (file_name):
+
+    file = open (file_name, 'r')
+
+    for line in file:
+        print (line.rstrip ('\n'))
+
+    file.close ()
+
+
+def write_employee_records (file_name):
+    file = open (file_name, 'w')
+
+    choice = '1'
+
+    while choice == '1':
+        id = input ('Enter ID: ')
+        name = input ('Enter name: ')
+        dept = input ('Enter dept: ')
+
+        file.write (id + '\t')
+        file.write (name + '\t')
+        file.write (dept + '\t')
+
+        choice = input ('Enter 1 to continue: ')
+
+    file.close ()
+
+def read_employee_records (file_name):
+    file = open (file_name, 'r')
+
+    for employee in file:
+        record = employee.split('\t') # this creates a list [ID, Name, Dept]
+
+        id = record [0]
+        name = record [1]
+        dept = record [2].rstrip('\n')
+
+        print (id, name, dept)
+
+    file.close ()
+
+
+def write_prog_lang_list_of_lists (file_name):
+    file = open (file_name, 'w')
+
+    prog_langs = [['1980', 'C++', 'Complex'], ['1996', 'Python', 'Easy'], ['1991', 'Java', 'Medium']]
+
+    for lang in prog_langs:
+        file.write(lang[0] + '\t') #year
+        file.write(lang[1] + '\t') #lang
+        file.write(lang[2] + '\t') #level
+
+    file.close ()
+
+
+def read_prog_lang_list_of_lists (file_name):
+    file = open (file_name, 'r')
+
+    list_lang = []
+
+    for line in file:
+        record = line.split ('\t')
+
+        year = record [0]
+        lang = record [1]
+        level = record [2].rstrip('\n')
+
+        lang_list = [year, lang, level]
+
+        list_lang.append (lang_list)
+
+    print (list_lang)
+
+    for lang in list_lang:
+        print (lang[0],lang[1],lang[2])
+
+    file.close ()
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,22 @@
 #main program
+
+import files
+
+def main ():
+
+    #file_name = 'philosophers.txt'
+    #files.write_to_file (file_name)
+    #files.read_from_file (file_name)
+    #files.read_from_file_one_line_at_a_time (file_name)
+    #files.read_from_file_data_w_while_loop (file_name)
+    #files.read_from_file_data_w_for_loop (file_name)
+
+    #file_name = 'employees.txt'
+    #files.write_employee_records (file_name)
+    #files.read_employee_records (file_name)
+
+    file_name = 'langs.txt'
+    files.write_prog_lang_list_of_lists (file_name)
+    #files.read_prog_lang_list_of_lists (file_name)
+
+main ()
