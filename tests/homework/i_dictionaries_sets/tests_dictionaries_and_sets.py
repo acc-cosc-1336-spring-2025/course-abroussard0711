89616845c4215f6d69b6b3d132dad1f0f434b3c5
abroussard0711 <alexandria.broussard@g.austincc.edu>
2025-04-13 04:30:01 +0000

    HW 8 abroussard0711 04.12.25 test update

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -17,11 +17,11 @@ class Test_Config(unittest.TestCase):
         add_inventory (inventory, 'Widget1', -10)
         self.assertEqual (inventory ['Widget1'], 25)
 
-def test_remove_widget_inventory(self):
+    def test_remove_widget_inventory(self):
         inventory = {'Widget1': 10,'Widget2': 20}
 
         result = remove_widget_inventory(inventory, 'Widget1')
-        self.assertEqual(result, "Record deleted")
+        self.assertEqual(result, "Record deleted.")
         self.assertEqual(len(inventory), 1)
         self.assertIn('Widget2', inventory)
         self.assertNotIn('Widget1', inventory)
